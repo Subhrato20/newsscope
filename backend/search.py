3547624b6ad08d078f31b8aba6dc87d557c5c3c9
Yixiang Chen <yee0730@gmail.com>
2025-06-18 11:59:35 -0400

    upload data to pathway

--- backend/search.py
+++ backend/search.py
@@ -1,6 +1,16 @@
 import asyncio
 import json
 from firecrawl import AsyncFirecrawlApp, ScrapeOptions
+import pathway as pw
+from pathway import debug
+
+class Article(pw.Schema):
+    title: str
+    description: str
+    url: str
+    markdown: str
+    metadata: dict
+    links: list[str]
 
 async def main():
     app = AsyncFirecrawlApp(api_key='fc-0d3f25709bd14df9924bad5a307be023')
@@ -14,5 +24,14 @@ async def main():
     with open("articles.jsonl", "w", encoding="utf-8") as f:
         for record in response["data"]:
             f.write(json.dumps(record) + "\n")
+    
+    articles = pw.io.jsonlines.read(
+        "articles.jsonl",
+        schema=Article,
+        mode="streaming" 
+    )
+
+    pw.debug.compute_and_print_update_stream(articles)
+    pw.run()
 
 asyncio.run(main())
